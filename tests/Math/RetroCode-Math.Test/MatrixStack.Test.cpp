54d24727992442472ebd36ebc655e23c6690e286
Olivier Choquet <48131940+OliveiraDF@users.noreply.github.com>
2025-02-20 16:14:15 +0100

    Initial commit

---
+++ tests/Math/RetroCode-Math.Test/MatrixStack.Test.cpp
@@ -0,0 +1,82 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RetroCodeMathTest
+{
+
+	TEST_CLASS(MatrixStackTest)
+	{
+	public:
+
+		TEST_METHOD(TestAllocate)
+		{
+			BOOL bIsDifferent = TRUE;
+
+			HRESULT hr = Allocate(8, bIsDifferent);
+
+			Assert::AreEqual(FALSE, bIsDifferent, _T("Memory leak"));
+			Assert::IsTrue(SUCCEEDED(hr), _com_error(hr).ErrorMessage());
+		}
+
+		TEST_METHOD(TestReallocate)
+		{
+			BOOL bIsDifferent = TRUE;
+
+			HRESULT hr = Allocate(24, bIsDifferent);
+
+			Assert::AreEqual(FALSE, bIsDifferent, _T("Memory leak"));
+			Assert::IsTrue(SUCCEEDED(hr), _com_error(hr).ErrorMessage());
+		}
+
+	private:
+
+		HRESULT Allocate(INT nPushCount, BOOL& bIsMemDifferent)
+		{
+			retro::memory::CMemoryLeakChecker MemoryLeakChecker;
+
+			MemoryLeakChecker.Begin();
+			{
+				retro::math::CMatrixStack MatrixStack;
+
+				for (INT i = 1; i <= nPushCount; i++)
+				{
+					const FLOAT fVal = static_cast<FLOAT>(i);
+					D2D1::Matrix3x2F* pTop = const_cast<D2D1::Matrix3x2F*>(MatrixStack.GetTop());
+
+					pTop->m11 = fVal;
+					pTop->m12 = fVal;
+					pTop->m21 = fVal;
+					pTop->m22 = fVal;
+					pTop->dx = fVal;
+					pTop->dy = fVal;
+
+					MatrixStack.Push();
+				}
+
+				for (INT i = nPushCount; i >= 1; i--)
+				{
+					const FLOAT fVal = static_cast<FLOAT>(i);
+
+					MatrixStack.Pop();
+
+					D2D1::Matrix3x2F mMatrix(fVal, fVal, fVal, fVal, fVal, fVal);
+					const D2D1::Matrix3x2F* pTop = MatrixStack.GetTop();
+
+					const INT nCompare = memcmp(&mMatrix, pTop, sizeof(D2D1::Matrix3x2F));
+					if (0 != nCompare)
+					{
+						return E_FAIL;
+					}
+				}
+			}
+
+			bIsMemDifferent = MemoryLeakChecker.End();
+
+			return S_OK;
+		}
+
+	};
+
+}
