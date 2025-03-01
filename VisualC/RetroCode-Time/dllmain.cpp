77d73759eab91ad5017c08a92c645bb6fe046350
Olivier Choquet <48131940+OliveiraDF@users.noreply.github.com>
2025-03-01 17:03:19 +0100

    Add RetroCode-Time module

---
+++ VisualC/RetroCode-Time/dllmain.cpp
@@ -0,0 +1,52 @@
+// dllmain.cpp : Définit les routines d'initialisation pour la DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <afxwin.h>
+#include <afxdllx.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+static AFX_EXTENSION_MODULE RetroCodeTimeDLL = { false, nullptr };
+
+extern "C" int APIENTRY
+DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	// Supprimez cet élément si vous utilisez lpReserved
+	UNREFERENCED_PARAMETER(lpReserved);
+
+	if (dwReason == DLL_PROCESS_ATTACH)
+	{
+		TRACE0("Initialisation de RetroCode-Time.DLL !\n");
+
+		// Initialisation unique de la DLL d'extension
+		if (!AfxInitExtensionModule(RetroCodeTimeDLL, hInstance))
+			return 0;
+
+		// Insérez cette DLL dans la chaîne de ressource
+		// REMARQUE : Si cette DLL d'extension est implicitement liée par
+		//  une DLL régulière MFC (par exemple un contrôle ActiveX)
+		//  au lieu d'une application MFC, supprimez
+		//  cette ligne de DllMain et placez-la dans une fonction
+		//  séparée exportée à partir de cette DLL d'extension.  La DLL régulière
+		//  qui utilise cette DLL d'extension doit ensuite explicitement appeler cette
+		//  fonction pour initialiser cette DLL d'extension.  Dans le cas contraire,
+		//  l'objet CDynLinkLibrary ne sera pas attaché à la chaîne de ressource
+		//  de la DLL régulière et de nombreux problèmes
+		//  seront générés.
+
+		new CDynLinkLibrary(RetroCodeTimeDLL);
+
+	}
+	else if (dwReason == DLL_PROCESS_DETACH)
+	{
+		TRACE0("Fin d'exécution de RetroCode-Time.DLL !\n");
+
+		// Terminez la bibliothèque avant que les destructeurs soient appelés
+		AfxTermExtensionModule(RetroCodeTimeDLL);
+	}
+	return 1;   // ok
+}
